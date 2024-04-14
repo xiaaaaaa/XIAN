bc7217695df66b20261c0b54faa53fc6467f52b0
Slionkyiu1129 <xinyuu9215@gmail.com>
2024-04-15 02:06:43 +0800

    詳細頁面更改中

--- goBus/scr/screens/DetailRouteSrceen.js
+++ goBus/scr/screens/DetailRouteSrceen.js
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet } from 'react-native';
-import { Text, View } from '@gluestack-ui/themed';
+import { Text, View, Box } from '@gluestack-ui/themed';
+import SegmentedControlTab from "react-native-segmented-control-tab";
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import DetailRoute from '../components/DetailRoute'
-import SegmentedControlTab from "react-native-segmented-control-tab";
+import data from "../json/BusRoute.json"
+
 
 const DetailRouteScreen = () => {
+    const [selectedIndex, setSelectedIndex] = useState(0);
+    const SegmentedContent = () => {
+        if (selectedIndex == 1) {
+            
+            
+        }else {
+            
+        }
+    };
     return (
         <View style={styles.container}>
             <Text style={styles.BusNumText}>18</Text>
-            <View style={styles.contentContainer}>
+
+                
+            <View style={styles.BusRouteContainer}>
                 <DetailRoute />
             </View>
         </View>
@@ -21,16 +34,20 @@ const styles = StyleSheet.create({
         height:'auto',
         width:'auto',
         marginTop: 25,
+        marginBottom: 90,
         justifyContent:'center',
         alignItems:'center'
     },
     BusNumText:{
         color:'#000000',
-        fontSize:24,
-        paddingBottom:5,
+        fontSize:48,
+        marginTop:10,
         marginLeft:'auto',
         marginRight:'auto',
     },
+    BusRouteContainer:{
+
+    }
 });
 
 export default DetailRouteScreen;
