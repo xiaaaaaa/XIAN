468cb490c6694b49dbe13cf14f5878aff1a54bcc
Slionkyiu1129 <xinyuu9215@gmail.com>
2024-04-14 21:29:58 +0800

    更新SearchScreen

--- goBus/scr/screens/SearchScreen.js
+++ goBus/scr/screens/SearchScreen.js
@@ -1,17 +1,53 @@
 import React from "react";
-import { StyleSheet } from 'react-native';
-import { Text } from '@gluestack-ui/themed';
+import { SearchBar } from 'react-native-elements';
+import { StyleSheet, View } from 'react-native';
 
-const SearchScreen = () => {
-    return(
-        <Text style={styles.text}>The Search Screen</Text>
-    );
+export default class Search extends React.Component {
+    state = {
+        search: '',
+    };
+
+    updateSearch = (search) => {
+        this.setState({ search });
+    };
+
+    render() {
+        const { search } = this.state;
+
+        return (
+            <View style={styles.container}>
+                <SearchBar
+                    placeholder="搜尋公車路線"
+                    onChangeText={this.updateSearch}
+                    value={search}
+                    containerStyle={{
+                        backgroundColor: 'white',
+                        borderBottomWidth: 0,
+                        borderTopWidth: 0,
+                        borderRadius: 14,
+                        paddingHorizontal: 10,
+                        marginTop: 10,
+                        width:400,
+                        height:50,
+
+                    }}
+                    inputContainerStyle={{
+                        backgroundColor: '#EFEFF0',
+                        margin: 5,
+                        borderRadius: 14,
+                    }}
+                />
+            </View>
+
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-    text:{
-        margin:20,
+    container:{
+        width:'100%',
+        height:'100%',
+        alignItems: 'center',
+        backgroundColor: 'white'
     }
 });
-
-export default SearchScreen;
