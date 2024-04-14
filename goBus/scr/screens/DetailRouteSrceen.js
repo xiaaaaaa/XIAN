ffac86500639f15ec557e1e4ef62ebca7cc267c0
Slionkyiu1129 <xinyuu9215@gmail.com>
2024-04-15 00:04:00 +0800

    新增詳細路線葉面

--- goBus/scr/screens/DetailRouteSrceen.js
+++ goBus/scr/screens/DetailRouteSrceen.js
@@ -1,19 +1,28 @@
 import React from "react";
 import { StyleSheet } from 'react-native';
-import { Text } from '@gluestack-ui/themed';
+import { Text, View } from '@gluestack-ui/themed';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import DetailRoute from '../components/DetailRoute'
 
 const DetailRouteScreen = () => {
-    return(
-        
-        <Text style={styles.text}>The DetailRoute Screen</Text>
+    return (
+        <View style={styles.container}>
+            <Text style={styles.BusNumText}>18往萬華</Text>
+            <View style={styles.contentContainer}>
+                <DetailRoute />
+            </View>
+        </View>
     );
 }
 
 const styles = StyleSheet.create({
-    text:{
-        margin:20,
-    }
+    BusNumText:{
+        color:'#000000',
+        fontSize:24,
+        paddingBottom:5,
+        marginLeft:'auto',
+        marginRight:'auto',
+    },
 });
 
 export default DetailRouteScreen;
