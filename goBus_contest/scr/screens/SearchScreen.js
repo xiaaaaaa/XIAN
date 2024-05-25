356c398aec087cf55fd271aefd0934617a36fe4c
Slionkyiu1129 <xinyuu9215@gmail.com>
2024-05-26 01:04:46 +0800

    變更搜尋功能

--- goBus_contest/scr/screens/SearchScreen.js
+++ goBus_contest/scr/screens/SearchScreen.js
@@ -1,90 +1,76 @@
-import React, { useState } from 'react';
-import { StyleSheet, Keyboard, Platform, View, Text, ScrollView, TouchableWithoutFeedback, SectionList, FlatList, } from 'react-native';
-import { SearchBar } from 'react-native-elements';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Keyboard, View, Text, TouchableWithoutFeedback, FlatList, TextInput, Pressable } from 'react-native';
 import BusRouteData from "../json/BusRoute.json";
-import SearchLoveRouteDetail from "../components/SearchLoveRouteDetail"
-import SearchRecordRouteDetail from "../components/SearchRecordRouteDetail"
+import { useNavigation } from '@react-navigation/native';
+import Icon from "react-native-vector-icons/AntDesign"
 
 const Search = () => {
+    const navigation = useNavigation();
+    const [data, setData] = useState(BusRouteData[0].data);
     const [search, setSearch] = useState('');
+    const [filteredData, setFilteredData] = useState([]);
+
+    useEffect(() => {
+        filterList(search);
+    }, [search]);
 
     const updateSearch = (search) => {
         setSearch(search);
     };
-
+    const filterList = (searchValue) => {
+        if (searchValue === '') {
+            setFilteredData([]);
+        } else {
+            let updatedList = data.filter((item) => {
+                return item.busNum.toString().indexOf(search.toString()) !== -1
+            })
+            setFilteredData(updatedList);
+        }
+    };
     const hideKeyboard = () => {
         Keyboard.dismiss();
     };
-    const renderSectionHeader1 = ({ section, navigation }) => (
-        <>
-            <FlatList
-                data={section.data}
-                renderItem={({ item }) => <SearchRecordRouteDetail busRoute={item} navigation={navigation} />}
-                showsHorizontalScrollIndicator={false}
-                keyExtractor={item => item.title}
-            />
-        </>
-    );
-    
-    const renderSectionHeader2 = ({ section, navigation }) => (
-        <>
-            <FlatList
-
-                data={section.data}
-                renderItem={({ item }) => <SearchLoveRouteDetail busRoute={item} navigation={navigation} />}
-                showsHorizontalScrollIndicator={false}
-                keyExtractor={item => item.title}
-            />
-        </>
-    );
-    const renderItem = ({ item, section, navigation }) => {
-        return null
+    const handleIconPress = () => {
+        console.log("press button");
     };
 
     return (
         <TouchableWithoutFeedback onPress={hideKeyboard}>
             <View style={styles.container}>
-                <SearchBar
-                    placeholder="搜尋公車路線"
-                    onChangeText={updateSearch}
-                    value={search}
-                    inputStyle={{ color: '#000' }}
-                    containerStyle={{
-                        backgroundColor: 'white',
-                        borderBottomWidth: 0,
-                        borderTopWidth: 0,
-                        borderRadius: 14,
-                        paddingHorizontal: 10,
-                        marginTop: 10,
-                        width: 410,
-                        height: 50,
-                    }}
-                    inputContainerStyle={{
-                        backgroundColor: '#EFEFF0',
-                        margin: 5,
-                        borderRadius: 14,
-                    }}
-                />
-                <View style={styles.content}>
-                    <Text style={styles.sectionTitle}>搜尋紀錄</Text>
-                    <SectionList
-                        sections={BusRouteData}
-                        contentContainerStyle={{ paddingHorizontal: 10 }}
-                        renderSectionHeader={renderSectionHeader1}
-                        renderItem={renderItem}
-                        keyExtractor={item => item.title}
-                    />
-                    <Text style={styles.line}></Text>
-                    <Text style={styles.sectionTitle}>最愛路線</Text>
-                    <SectionList
-                        sections={BusRouteData}
-                        contentContainerStyle={{ paddingHorizontal: 10 }}
-                        renderSectionHeader={renderSectionHeader2}
-                        renderItem={renderItem}
-                        keyExtractor={item => item.title}
+                <View style={styles.searchspace}>
+                    <Icon name={'search1'} size={20} style={styles.icon} />
+                    <TextInput
+                        onChangeText={updateSearch}
+                        value={search}
+                        type="text"
+                        style={styles.Input}
+                        placeholder='搜尋公車路線' />
+                </View>
+                <View>
+                    <Text style={styles.searchTitle}>搜尋紀錄</Text>
+                    <FlatList
+                        data={filteredData.length > 0 ? filteredData : data}
+                        renderItem={({ item }) =>
+                            <View style={styles.searchResultContain}>
+
+                                <View >
+                                    <Text style={styles.searchResult}>{item.busNum}</Text>
+                                    <Text style={styles.searchResultDetail}>{item.detail[0].startEndStation}</Text>
+                                </View>
+
+                                <View style={styles.searchResultContent}>
+                                    <Icon name={'heart'} size={20} style={styles.heart} />
+                                    <Pressable onPress={() => navigation.navigate('DetailRoute')}>
+                                        <Icon name={'right'} size={20} style={styles.right} />
+                                    </Pressable>
+                                </View>
+
+                            </View>
+                        }
+                        keyExtractor={(item) => item.busNum}
                     />
                 </View>
-
+                
             </View>
         </TouchableWithoutFeedback>
     );
@@ -93,7 +79,6 @@ const Search = () => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-
         backgroundColor: 'white'
     },
     content: {
@@ -103,16 +88,73 @@ const styles = StyleSheet.create({
     },
     sectionTitle: {
         margin: 30,
-        marginBottom:5,
+        marginBottom: 5,
         fontSize: 18,
         textAlign: 'right'
     },
-    line:{
-        width:350,
-        height:2,
-        backgroundColor:'#C4D7F3',
-        marginLeft:25
-    }
+    line: {
+        width: 350,
+        height: 2,
+        backgroundColor: '#C4D7F3',
+        marginLeft: 25
+    },
+    searchspace: {
+        flexDirection: 'row',
+        alignItems: 'center',
+      },
+      icon: {
+        color: '#848488',
+        backgroundColor: '#EFEFF0',
+        height: 35,
+        marginLeft: '10%',
+        marginTop: '4%',
+        paddingTop: 7,
+        paddingLeft: 5,
+        borderTopLeftRadius: 10,
+        borderBottomLeftRadius: 10,
+      },
+      Input: {
+        backgroundColor: '#EFEFF0',
+        borderTopRightRadius: 10,
+        borderBottomRightRadius: 10,
+        height: 35,
+        width: 300,
+        marginTop: '4%',
+        fontSize: 18,
+        paddingLeft: 10
+      },
+      searchTitle: {
+        fontSize: 18,
+        marginTop: '4%',
+        marginLeft: '10%',
+      },
+      searchResultContain: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginBottom:10
+      },
+      searchResultContent: {
+        flexDirection: 'row'
+      },
+      searchResult: {
+        fontSize: 18,
+        marginTop: 10,
+        marginLeft: 40,
+      },
+      searchResultDetail: {
+        marginLeft: 40,
+        marginTop: 5,
+      },
+      heart: {
+        color: '#EBAFA3',
+        height: 35,
+        marginTop: 23,
+        marginRight: '15%',
+      },
+      right: {
+        color: '#C4D7F3',
+        marginTop: 23,
+      }
 });
 
 export default Search;
