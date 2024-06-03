c36b3a1227bf9a27ef48a370d578541aaf9fc7d8
Slionkyiu1129 <xinyuu9215@gmail.com>
2024-06-04 00:45:55 +0800

    新增小動畫-公車icon

--- GoBus_contest_SDK51/components/components/HomeBusDetailCardDetail.js
+++ GoBus_contest_SDK51/components/components/HomeBusDetailCardDetail.js
@@ -1,27 +1,57 @@
-import React, { useState } from "react";
-import { StyleSheet, View, Text, Image, Pressable, Linking, Platform} from "react-native";
+import React, { useState, useEffect } from "react";
+import { StyleSheet, View, Text, Image, Pressable, Linking, Platform } from "react-native";
 import SegmentedControlTab from "react-native-segmented-control-tab";
 import { useNavigation } from '@react-navigation/native';
 import { HStack, Box, Center, VStack } from "@gluestack-ui/themed";
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import Animated, {
+    Easing,
+    useSharedValue,
+    useAnimatedStyle,
+    withSpring,
+    withSequence,
+    withRepeat,
+    withTiming,
+} from "react-native-reanimated";
 
 const HomeBusDetailCardDetail = (props) => {
-    const {data} = props.busDetail;
+    const { data } = props.busDetail;
     const [selectedIndex, setSelectedIndex] = useState(0);
     let station1 = data[0].routes[0].busRoute;
-    let station2 =  data[0].routes[1].busRoute;
+    let station2 = data[0].routes[1].busRoute;
+    //animation
+    const translateX = useSharedValue(0);
+    const animatedStyles = useAnimatedStyle(() => ({
+        transform: [{ translateX: translateX.value }],
+    }));
+
+    const shake = () => {
+        translateX.value = withSequence(
+            withTiming(-5, { duration: 50, easing: Easing.ease }),
+            withTiming(5, { duration: 50, easing: Easing.ease }),
+            withTiming(-5, { duration: 50, easing: Easing.ease }),
+            withTiming(0, { duration: 50, easing: Easing.ease })
+        );
+    };
 
     const SegmentedContent = () => {
         if (selectedIndex == 1) {
             return (
-                <Center flex={1}  style={styles.busState}>
-                    <Image 
+                <Center flex={1} style={styles.busState}>
+                    <Image
                         style={styles.road}
                         source={require('../image/road.png')}
                     />
                     <HStack style={styles.detailCardText}>
                         <Center style={styles.arrivalBus}>
-                            <MaterialCommunityIcons name="bus-side" color={'#2D2F31'} size={49}  style={styles.busIcon}/>
+                            <Animated.View style={animatedStyles}>
+                                <Pressable
+                                    onPress={() => {
+                                        shake();
+                                    }}>
+                                    <MaterialCommunityIcons name="bus-side" color={'#2D2F31'} size={49} style={styles.busIcon} />
+                                </Pressable>
+                            </Animated.View>
                             <Text style={styles.arrivalBusNum}>{data[0].routes[1].arrivalBusNum}</Text>
                         </Center>
                         <Center style={styles.arrivalTimeCard}>
@@ -36,14 +66,21 @@ const HomeBusDetailCardDetail = (props) => {
             )
         } else {
             return (
-                <Center flex={1}  style={styles.busState}>
-                    <Image 
+                <Center flex={1} style={styles.busState}>
+                    <Image
                         style={styles.road}
                         source={require('../image/road.png')}
                     />
                     <HStack style={styles.detailCardText}>
                         <Center style={styles.arrivalBus}>
-                            <MaterialCommunityIcons name="bus-side" color={'#2D2F31'} size={49}  style={styles.busIcon}/>
+                            <Animated.View style={animatedStyles}>
+                                <Pressable
+                                    onPress={() => {
+                                        shake();
+                                    }}>
+                                    <MaterialCommunityIcons name="bus-side" color={'#2D2F31'} size={49} style={styles.busIcon} />
+                                </Pressable>
+                            </Animated.View>
                             <Text style={styles.arrivalBusNum}>{data[0].routes[0].arrivalBusNum}</Text>
                         </Center>
                         <Center style={styles.arrivalTimeCard}>
@@ -59,46 +96,46 @@ const HomeBusDetailCardDetail = (props) => {
         }
     }
 
-    return(
+    return (
         <View style={styles.busDetailCard}>
             <HStack style={styles.route}>
                 <Text style={styles.busNum}>{data[0].busNum}</Text>
                 <Text style={styles.toText}>往</Text>
                 <Box flex={1}>
-                    <SegmentedControlTab 
+                    <SegmentedControlTab
                         values={[station1, station2]}
                         activeTabStyle={{
-                            width:194,
-                            height:53,
-                            backgroundColor: "#fff",   
+                            width: 194,
+                            height: 53,
+                            backgroundColor: "#fff",
                             borderColor: '#C4D7F3',
-                            shadowColor:'#000',
+                            shadowColor: '#000',
                             shadowOffset: Platform.OS === 'ios' ? { width: 0, height: 5 } : { width: 0, height: 20 },
                             shadowOpacity: 0.1,
                             // Android Only
                             elevation: 3,
                         }}
-                        tabStyle={{ 
-                            width:194,
-                            height:53,
+                        tabStyle={{
+                            width: 194,
+                            height: 53,
                             backgroundColor: "#C4D7F3",
                             borderColor: "#C4D7F3",
-                            borderWidth:1,
+                            borderWidth: 1,
                         }}
-                        firstTabStyle={{ marginLeft: 10,  borderTopLeftRadius:9,borderBottomLeftRadius:9,}}
-                        lastTabStyle={{ marginRight: 30, borderTopRightRadius:9, borderBottomRightRadius:9,}}
+                        firstTabStyle={{ marginLeft: 10, borderTopLeftRadius: 9, borderBottomLeftRadius: 9, }}
+                        lastTabStyle={{ marginRight: 30, borderTopRightRadius: 9, borderBottomRightRadius: 9, }}
                         textNumberOfLines={2}
-                        tabTextStyle={{ 
-                            fontSize: 16, color: "#354967", borderWidth:0,fontWeight:'500',
+                        tabTextStyle={{
+                            fontSize: 16, color: "#354967", borderWidth: 0, fontWeight: '500',
                             textAlign: 'center',
-                            lineHeight:20,
-                            padding:0,
-                            paddingVertical:0,
+                            lineHeight: 20,
+                            padding: 0,
+                            paddingVertical: 0,
                         }}
-                        activeTabTextStyle={{ color: "#000",}}
+                        activeTabTextStyle={{ color: "#000", }}
                         selectedIndex={selectedIndex}
                         onTabPress={(index) => setSelectedIndex(index)}
-                        
+
                     />
                 </Box>
             </HStack>
@@ -108,91 +145,91 @@ const HomeBusDetailCardDetail = (props) => {
 }
 
 const styles = StyleSheet.create({
-    busDetailCard:{
-        width:350,
-        height:190,
-        backgroundColor:'#E1E9F7',
-        display:'flex',
-        marginTop:30,
-        marginBottom:15,
-        marginLeft:'auto',
-        marginRight:'auto',
-        borderColor:'#fff',
-        borderWidth:0,
-        borderRadius:20,
+    busDetailCard: {
+        width: 350,
+        height: 190,
+        backgroundColor: '#E1E9F7',
+        display: 'flex',
+        marginTop: 30,
+        marginBottom: 15,
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        borderColor: '#fff',
+        borderWidth: 0,
+        borderRadius: 20,
     },
-    route:{
-        marginTop:3,
+    route: {
+        marginTop: 3,
         // height:900,
-        display:'flex',
-        justifyContent:'center',
-        alignItems:'center',
-        borderWidth:0,
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderWidth: 0,
     },
-    SegmentedControl:{
-        display:'flex', flexWrap:'wrap'
+    SegmentedControl: {
+        display: 'flex', flexWrap: 'wrap'
     },
-    busNum:{
-        paddingLeft:35,
-        paddingRight:35,
-        fontSize:55,
+    busNum: {
+        paddingLeft: 35,
+        paddingRight: 35,
+        fontSize: 55,
     },
-    toText:{
-        fontSize:20,
-        paddingBottom:4,
+    toText: {
+        fontSize: 20,
+        paddingBottom: 4,
     },
-    busState:{
-        marginTop:60,
+    busState: {
+        marginTop: 60,
     },
-    road:{
+    road: {
 
     },
-    detailCardText:{
-        marginTop:-63,
-        
+    detailCardText: {
+        marginTop: -63,
+
     },
-    arrivalBus:{
+    arrivalBus: {
         // borderColor:'#000',
         // borderWidth:1,
-        marginRight:20,
+        marginRight: 20,
     },
-    busIcon:{
-        marginBottom:-6
+    busIcon: {
+        marginBottom: -6
     },
-    arrivalBusNum:{
-        backgroundColor:'#E1E9F7',
-        fontSize:16,
-        borderColor:'#000',
-        borderWidth:1,
-        borderRadius:6,
-        paddingHorizontal:10,
-        paddingVertical:3,
+    arrivalBusNum: {
+        backgroundColor: '#E1E9F7',
+        fontSize: 16,
+        borderColor: '#000',
+        borderWidth: 1,
+        borderRadius: 6,
+        paddingHorizontal: 10,
+        paddingVertical: 3,
     },
-    arrivalTimeCard:{
+    arrivalTimeCard: {
         // borderColor:'#000',
         // borderWidth:1,
     },
-    SiteName:{
-        fontSize:16,
-        marginBottom:10
+    SiteName: {
+        fontSize: 16,
+        marginBottom: 10
     },
-    arrivalTime:{
-        backgroundColor:'#E1E9F7',
-        borderColor:'#000',
-        borderWidth:1,
-        borderRadius:6,
-        paddingHorizontal:10,
-        paddingVertical:4,
-        display:'flex',
-        alignItems:'baseline'
+    arrivalTime: {
+        backgroundColor: '#E1E9F7',
+        borderColor: '#000',
+        borderWidth: 1,
+        borderRadius: 6,
+        paddingHorizontal: 10,
+        paddingVertical: 4,
+        display: 'flex',
+        alignItems: 'baseline'
     },
-    arrivalTimeNum:{
-        fontSize:24,
-        paddingRight:7
+    arrivalTimeNum: {
+        fontSize: 24,
+        paddingRight: 7
     },
-    unit:{
-        fontSize:16,
-        
+    unit: {
+        fontSize: 16,
+
     }
 });
 
