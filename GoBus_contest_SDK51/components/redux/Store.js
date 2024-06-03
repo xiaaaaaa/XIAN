99b9e0ef084c05277b3b0fafa30c54f9fc5a6f72
Slionkyiu1129 <xinyuu9215@gmail.com>
2024-06-03 22:25:49 +0800

    新增儲存到本地，打除了XIAN的名字，可以再增加遺失物頁看到，重新reload也可以呈現剛剛打得

--- GoBus_contest_SDK51/components/redux/Store.js
+++ GoBus_contest_SDK51/components/redux/Store.js
@@ -1,13 +1,24 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from './Slice';
+import { persistStore, persistReducer } from 'redux-persist';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+const persistConfig = {
+    key: 'root',
+    storage: AsyncStorage,
+  };
 
 // Part2: Combine Reducers and Create a Store
 const store = configureStore({
    reducer: {
-     counter: counterReducer
+    counter: persistReducer(persistConfig, counterReducer),
    },
    devTools: process.env.NODE_ENV !== 'production',
- });
+   middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+  });
+  persistStore(store);
 
 //  export store to global
 export default store;
